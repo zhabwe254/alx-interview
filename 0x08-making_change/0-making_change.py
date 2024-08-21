464a8202562e6ac6e8b5f38069ccb415cbf93b43
zhabwe254 <zhabwe@gmail.com>
2024-08-21 09:58:27 +0300

    P

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
 def makeChange(coins, total):
+    """
+    Determine the fewest number of coins needed to meet the given total.
+    
+    Args:
+        coins (list): List of coin denominations.
+        total (int): The total amount to be met.
+        
+    Returns:
+        int: Fewest number of coins needed to meet the total.
+             If the total cannot be met by any number of coins, return -1.
+    """
     if total <= 0:
         return 0
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            if i - coin >= 0:
-                dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    
+    # Sort the coin denominations in descending order
+    coins.sort(reverse=True)
+    
+    num_coins = 0
+    for coin in coins:
+        if total == 0:
+            break
+        # Determine the maximum number of coins of this denomination
+        num_coins += total // coin
+        # Reduce the total by the corresponding amount
+        total %= coin
+    
+    # If the total is not zero, we couldn't meet it with the available coins
+    if total != 0:
+        return -1
+    
+    return num_coins
