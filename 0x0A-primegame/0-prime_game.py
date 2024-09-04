d8c2e7b06e2e9138ee0aa81df48570761b9c88c3
zhabwe254 <zhabwe@gmail.com>
2024-09-04 12:16:17 +0300

    a

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,49 +1,47 @@
 #!/usr/bin/python3
-
-def is_prime(n):
-    """Check if a number is prime"""
-    if n <= 1:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
-
-def remove_multiples(n, nums):
-    """Remove multiples of n from the list of numbers"""
-    return [num for num in nums if num % n != 0]
-
-def play_round(n):
-    """Play a round of the game"""
-    nums = list(range(1, n + 1))
-    winner = None
-    while True:
-        if not any(is_prime(num) for num in nums):
-            break
-        if winner is None:
-            for num in nums:
-                if is_prime(num):
-                    nums = remove_multiples(num, nums)
-                    winner = "Maria"
-                    break
-        else:
-            for num in nums:
-                if is_prime(num):
-                    nums = remove_multiples(num, nums)
-                    winner = "Ben"
-                    break
-    return winner
+# 0-prime_game.py
 
 def isWinner(x, nums):
-    """Determine the winner of the game"""
+    def is_prime(num):
+        if num <= 1:
+            return False
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
+
+    # Generate a sieve to find prime numbers up to the max number in nums
+    def generate_primes(n):
+        primes = []
+        sieve = [True] * (n + 1)
+        for p in range(2, n + 1):
+            if sieve[p]:
+                primes.append(p)
+                for multiple in range(p * p, n + 1, p):
+                    sieve[multiple] = False
+        return primes
+
+    # Game simulation function
+    def play_game(n):
+        primes = generate_primes(n)
+        taken = [False] * (n + 1)
+        moves = 0
+        for prime in primes:
+            if not taken[prime]:
+                moves += 1
+                for multiple in range(prime, n + 1, prime):
+                    taken[multiple] = True
+        return moves % 2 == 1  # If odd moves, Maria wins, else Ben wins
+
     maria_wins = 0
     ben_wins = 0
+
     for n in nums:
-        winner = play_round(n)
-        if winner == "Maria":
+        if play_game(n):
             maria_wins += 1
-        elif winner == "Ben":
+        else:
             ben_wins += 1
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
