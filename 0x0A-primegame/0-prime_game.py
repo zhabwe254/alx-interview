f3a0f94ccd9ba1598966e14b002a926cdb20e009
zhabwe254 <zhabwe@gmail.com>
2024-09-04 13:22:36 +0300

    p

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,36 +3,30 @@
 Prime Game Module
 """
 
+
 def isWinner(x, nums):
     """
     Determine the winner of the Prime Game.
-    
+
     :param x: number of rounds
     :param nums: array of n for each round
     :return: name of the player that won the most rounds, or None if it's a tie
     """
-    if not nums or x < 1:
+    if not nums or x <= 0:
         return None
-    
+
     max_num = max(nums)
-    primes = [True] * (max_num + 1)
+    primes = [True for _ in range(max(max_num + 1, 2))]
     primes[0] = primes[1] = False
-    
-    for i in range(2, int(max_num**0.5) + 1):
+
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
         if primes[i]:
-            for j in range(i*i, max_num + 1, i):
+            for j in range(i * i, max_num + 1, i):
                 primes[j] = False
-    
-    maria_wins = 0
-    ben_wins = 0
-    
-    for n in nums:
-        prime_count = sum(primes[:n+1])
-        if prime_count % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
-    
+
+    maria_wins = sum(sum(primes[2:n+1]) % 2 == 1 for n in nums)
+    ben_wins = x - maria_wins
+
     if maria_wins > ben_wins:
         return "Maria"
     elif ben_wins > maria_wins:
